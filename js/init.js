499efd74e398bf22c8a0b862722da70f8ef35929
Striz <peter.striz@gmail.com>
2017-05-17 13:01:11 +0100

    Update init.js
    hide navbar on scroll

--- js/init.js
+++ js/init.js
@@ -10,3 +10,21 @@
 /**$(document).ready(function(){
     $('.scrollspy').scrollSpy();
   }); **/
+
+// hide navbar on scroll down
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
+mywindow.scroll(function () {
+    newscroll = mywindow.scrollTop();
+    if (newscroll > mypos && !up) {
+        $('.navBar').stop().slideToggle(300);
+        up = !up;
+        console.log(up);
+    } else if(newscroll < mypos && up) {
+        $('.navBar').stop().slideToggle(300);
+        up = !up;
+    }
+    mypos = newscroll;
+});
